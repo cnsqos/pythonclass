d8e7b3fb70d6770dcb04bdf183aaf9ae6923bd6d
cnsqo <cnsqo0815@gmail.com>
2025-12-16 10:55:31 +0900

    add
    add

--- day1215/01_print.py
+++ day1215/01_print.py
@@ -1,3 +1,14 @@
+'''
+설정 - mouse 검색 - mouse wheel zoom 체크
+ctrl 마우스휠 (코드창만 확대/축소)
+
+ctrl shift P - keyboard shortcuts - run python file 검색 - 단축키 ctrl + f7
+'''
+
+#ctrl +
+#ctrl -
+#ctrl shift p -> keyboard short cuts
+
 print('hello')
 print('hi')
 print('hello, world')
