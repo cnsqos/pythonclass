f749989ff608392131009b159d2026b597e0f893
cnsqo <cnsqo0815@gmail.com>
2025-12-19 14:52:46 +0900

    add
    add

--- day1219/05_txtfile.py
+++ day1219/05_txtfile.py
@@ -13,10 +13,45 @@ if not os.path.exists(path):
     os.makedirs(path)
 
 
-f = open("./day1219/새파일.txt",'w', encoding="utf-8")
+# f = open("./day1219/새파일.txt",'w', encoding="utf-8")
 
-for i in range(1,11):
-    data = f'{i}번째 줄입니다.\n'
-    f.write(data) #메모장에 쓸 때는 문자열을 넣어야 한다.
+# for i in range(1,11):
+#     data = f'{i}번째 줄입니다.\n'
+#     f.write(data) #메모장에 쓸 때는 문자열을 넣어야 한다.
+# f.close()
+
+
+
+# print("\n======== 읽기 모드 (readline) ========\n")
+
+# f = open("./day1219/새파일.txt",'r',encoding="utf-8")
+# line = f.readline() # 첫 번째 출을 반환
+# print(line)
+# f.close()
+
+
+
+
+# print("\n======== while 문으로 여러줄 읽기 ========\n")
+
+# f = open("./day1219/새파일.txt",'r',encoding="utf-8")
+# while True:
+#     line = f.readline() # 첫 번째 출을 반환
+#     if not line: break
+#     print(line)
+# f.close()
+
+
+
+print("\n======== readlines 로 읽기 ========\n")
+
+f = open("./day1219/새파일.txt",'r',encoding="utf-8")
+lines = f.readlines() # 첫 번째 출을 반환
+print(lines)
+
+for line in lines:
+    line = line.strip()
+    print(line)
+
 f.close()
 
