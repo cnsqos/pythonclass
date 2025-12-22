5e8ce04ede812816b5ccff7bcc94285dba4afea3
cnsqo <cnsqo0815@gmail.com>
2025-12-22 17:12:24 +0900

    add
    add

---
+++ 08_python_function3.py
@@ -0,0 +1,18 @@
+print("\n ======== pow ========")
+
+print(pow(2, 4)) #16
+print(pow(2, 100))
+
+
+print("\n ======== range ========")
+
+print(list(range(5)))
+print(list(range(5,10)))
+
+print(list(range(1,10,2)))
+
+print(list(range(0,-11,-2)))
+print(list(range(0,-11,-1)))
+
+
+
--- day1222/07_python_function2.py
+++ day1222/07_python_function2.py
@@ -14,7 +14,69 @@ print(oct(16))
 
 print("\n ======== id ========")
 
-print(id())
-print(id())
-print(id())
-print(id())
+a = 3
+print(id(3))
+print(id(a))
+
+b = a
+print(id(b))
+print(id(4))
+
+
+
+print("\n ======== int  ========") # 정수로 반환
+
+print(int('3')) # 3
+print(int(3.4)) # 3
+print(int(3.9)) # 3
+
+print(int('11',2)) # 2진수 11을 10진수로 반환 # 3
+print(int('1A',16)) # 16진수 1A를 10진수로 반환 #26
+
+
+print("\n ======== isinstance  ========") # 그 클래스의 인스턴스인가?
+
+class Person: pass
+
+a = Person()
+b = 3
+
+print(isinstance(a, Person)) # True
+print(isinstance(b, Person)) # False
+
+
+print("\n ======== len  ========") # 길이 반환
+
+print(len('python'))
+print(len([1,2,3]))
+print(len((1,'a')))
+
+
+print("\n ======== list  ========")
+
+print(list('python'))
+print(list((1,2,3)))
+
+
+a = [1,2,3]
+b = a # 참조
+c = list(a) # 복사
+
+print()
+
+print(a)
+print(b)
+print(c)
+print(id(a)) 
+print(id(b))
+print(id(c))
+
+
+print("\n ======== max / min  ========")
+
+print(max([1,2,3])) #3
+print(max("python")) #y
+
+print(min([1,2,3])) #1
+print(min("python")) #h
+
