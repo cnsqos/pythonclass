d9203c276e8e8cd6800b2be99db61c8ca21bd829
cnsqo <cnsqo0815@gmail.com>
2025-12-24 18:04:56 +0900

    add
    add

--- day1224/08_df_oper.py
+++ day1224/08_df_oper.py
@@ -1,6 +1,7 @@
 #pip install seaborn
 
 import pandas as pd
+import seaborn as sns
 
 pd.set_option('display.unicode.east_asian_width', True)
 pd.set_option('display.max_columns', None)
@@ -8,3 +9,33 @@ pd.set_option('display.max_rows', 200)
 pd.set_option('display.width', 200)
 
 
+# 타이타닉 데이터 불러오기
+
+titanic = sns.load_dataset('titanic')
+print(titanic.head(100))
+titanic.info()
+
+# survived  생존여부 (1 / 0)
+# pclass    선실 등급 (숫자형)
+# sex       성별
+# age       나이
+# sibsp     함께 탑승한 형제/자매/배우자 수
+# parch     함께 탑승함 부모/자녀 수
+# fare      탑승요금
+# embarked  탑승항구 (C = Cherbourg, Q = Queenstown, S = Southampton)
+# class     선실 등급 (문자형)
+# who       승객 구분 (man, woman, child)
+# adult_male  성인 남성 (True, False)
+# deck      선실 위치
+# embark_town  탑승도시 이름 (Cherbourg, Queenstown, Southampton)
+# alive     생존여부 (yes / no)
+# alone     혼자? (True / False)
+
+
+#loc 사용 age, fare 만 뽑기
+
+
+df = titanic.loc[:, ['age', 'fare']]
+print(df)
+print()
+print(df.head())
