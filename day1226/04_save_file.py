03e120925f5ad31a4bfcabeada6e267eccea2877
cnsqo <cnsqo0815@gmail.com>
2025-12-26 15:40:53 +0900

    add
    add

---
+++ day1226/04_save_file.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+
+
+data = {'name' : [ 'Jerry', 'Riah', 'Paul'],
+        'algol' : [ 'A', 'A+', 'B'],
+        'basic' : [ 'C', 'B', 'B+'],
+        'c++' : [ 'B+', 'C', 'C+']}
+
+
+df = pd.DataFrame(data)
+print(df)
+print()
+
+df = df.set_index('name')
+print(df)
+print()
+
+print('\n======== CSV로 저장하기 ===========\n')
+
+df.to_csv("./data/df_sample.csv")
+print("df_sample.csv 저장완료")
+print()
+
+
+print('\n======== JSON으로 저장하기 ===========\n')
+
+df.to_json("./data/df_sample.json")
+print("df_sample.json 저장완료")
+
+
+# 인덱스, 컬럼, 데이터 분리저장
+# records index columns values
+
+
+df.to_json("./data/df_sample_split.json", orient='split')
+print("df_sample_spilt.json 저장완료")
+print()
+
+df.to_json("./data/df_sample_records.json", orient='records')
+print("df_sample_records.json 저장완료")
+print()
+
+df.to_json("./data/df_sample_index.json", orient='index')
+print("df_sample_index.json 저장완료")
+print()  
+
+df.to_json("./data/df_sample_columns.json", orient='columns')
+print("df_sample_columns.json 저장완료")
+print()
+
+df.to_json("./data/df_sample_values.json", orient='values')
+print("df_sample_values.json 저장완료")
+print()
+
+
+print('\n======== Excel으로 저장하기 ===========\n')
+
+df.to_excel("./data/df_sample.xlsx")
+print("df_sample.xlsx 저장완료")
+print()
+
+
+df.to_excel("./data/df_sample_no_index.xlsx", index = False)
+print("df_sample_no_index.xlsx 저장 완료")
+print()
+
+
+print('\n======== Excel으로 저장하기 ===========\n')
+
+
+
+data1 = {'name' : [ 'Jerry', 'Riah', 'Paul'],
+         'algol' : [ 'A', 'A+', 'B'],
+         'basic' : [ 'C', 'B', 'B+'],
+         'c++' : [ 'B+', 'C', 'C+']}
+
+data2 = {'c0':[1, 2, 3],
+         'c1':[4, 5, 6],
+         'c2':[7, 8, 9],
+         'c3':[10, 11, 12]}
+
+
+df1 = pd.DataFrame(data1)
+df1 = df1.set_index('name')
+
+df2 = pd.DataFrame(data2)
+df2 = df2.set_index('c0')
+
+print(df1)
+print()
+print(df2)
+print()
+
+
