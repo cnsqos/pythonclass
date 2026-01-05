429a2bc87a502ce30b8a57ad4eac6185bfb6c3c3
cnsqo <cnsqo0815@gmail.com>
2026-01-05 17:14:04 +0900

    add
    add

--- day260105/06_groupby_agg.py
+++ day260105/06_groupby_agg.py
@@ -38,4 +38,65 @@ print()
 # 그룹화 -> 컬럼선택 -> 집계
 std_fare = grouped['fare'].std(numeric_only=True)
 print(std_fare)
+print()
+
+std_age_survived = grouped[['age','survived']].std(numeric_only=True)
+print(std_age_survived)
+print()
+
+print('\n============= 그룹화 후 describe ============\n')
+
+print(grouped.describe())
+print()
+print(df.describe())
+
+print('\n============= 그룹화 후 value_counts ============\n')
+
+
+print(grouped[['class','sex']].value_counts())
+print()
+
+# 원래 value_counts 작동
+print(df['class'].value_counts())
+print()
+
+# grouped와 비교
+print(df[['class', 'sex']].value_counts())
+print()
+
+
+print('\n======= agg 메서드 =======\n')
+
+# 여러 함수나 컬럼별 다른 함수를 쓰기 위한 agg 메서드
+# aggregate() 애그리거트 - 합계, 총합...
+
+# 그룹 객체에 aggregate() 메서드 적용
+
+agg_mean = grouped.aggregate('mean', numeric_only = True)
+print(agg_mean)
+print()
+
+# agg 로도 쓸 수 있다
+agg_mean2 = grouped.agg('mean', numeric_only = True)
+print(agg_mean2)
+
+# 여러 집계함수를 적용
+agg_all = grouped.agg(['min','max'])
+print(agg_all)
+print()
+
+# 더 구체적으로 적용
+agg_sep = grouped.agg({'fare':['min','max'],'age':'mean'})
+print(agg_sep)
+print()
+
+
+# 함수를 만들어 적용
+
+def min_max(x):
+    return x.max() - x.min()
+
+agg_minmax = grouped[['age','fare']].agg(min_max)
+print(agg_minmax)
+print()
 
