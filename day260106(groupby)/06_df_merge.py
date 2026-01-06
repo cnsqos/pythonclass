04c6bc2696d3202326a4b99ffa1b11f07d45dd6b
cnsqo <cnsqo0815@gmail.com>
2026-01-06 16:15:20 +0900

    add
    add

--- day260106(groupby)/06_df_merge.py
+++ day260106(groupby)/06_df_merge.py
@@ -12,3 +12,75 @@ PBR (Price Book-value Ratio, 주가순자산비율)
 자산 대비 주가수준
 '''
 
+import pandas as pd
+pd.set_option('display.unicode.east_asian_width', True)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 300)
+
+# 주가 데이터 (stock price)
+df1 = pd.read_excel('data/stock_price.xlsx')
+print(df1)
+print()
+
+# 주식 가치평가 데이터 (stock_valuation)
+df2 = pd.read_excel('data/stock_valuation.xlsx')
+print(df2)
+print()
+
+# 데이터프레임 합치기 - 교집합
+merge_inner = pd.merge(df1, df2, how='inner', on='id')
+print(merge_inner)
+print()
+
+
+print('\n======= 데이터프레임 합치기 - 교집합 ========\n')
+
+
+merge_inner2 = pd.merge(df1, df2, how='inner', left_on=['id', 'stock_name'], right_on=['id','name'])
+'''
+id <-> id
+stock_name < - > name
+두가지 항목이 모두 같아야 하는 이너머지.
+ex) '종근딩' 으로 바꾸면 종근당 != 종근딩 이어서 해당 항목은 빠짐
+
+'''
+print(merge_inner2)
+print()
+
+
+print('\n======= 데이터프레임 합치기 - 합집합 ========\n')
+
+# 데이터프레임 합치기 - 합집합
+merge_outer = pd.merge(df1, df2, how = 'outer', on='id')
+print(merge_outer)
+print()
+
+# 왼쪽 기준으로 합치기
+merge_left = pd.merge(df1, df2, how = 'left', on = 'id')
+print(merge_left)
+print()
+
+# 오른쪽 기준으로 합치기
+merge_right = pd.merge(df1, df2, how = 'right', on = 'id')
+print(merge_right)
+print()
+
+# 교차 조인
+merge_cross = pd.merge(df1, df2, how='cross')
+print(merge_cross)
+print()
+
+# df1 에서 price가 5000 미만인 행들만 필터링
+price = df1[df1['price'] < 5000]
+print(price)
+print()
+
+value = pd.merge(price, df2, on = 'id')
+print(value)
+print()
+
+# 두개를 이너 머지 한 상태에서 price < 50000 으로 필터링 한 것과 같다.
+
+value2 = merge_inner[merge_inner['price'] < 5000]
+print(value2)
+print()
