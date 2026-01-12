3b6badee1908cf632f0ebc3f455ad01349dd7a50
cnsqo <cnsqo0815@gmail.com>
2026-01-12 11:32:09 +0900

    add
    add

---
+++ day260112/graph.py
@@ -0,0 +1,14 @@
+# pip install matplotlib
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from matplotlib import font_manager, rc
+#한글표기
+font_path = 'C:/Windows/Fonts/malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+# 음수표기
+plt.rcParams['axes.unicode_minus'] = False
+
+
