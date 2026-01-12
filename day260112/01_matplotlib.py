03d0d01c4a08c619824bdf663f2bc37e20331e85
cnsqo <cnsqo0815@gmail.com>
2026-01-12 15:01:14 +0900

    add
    add

---
+++ day260112/01_matplotlib.py
@@ -0,0 +1,47 @@
+# pip install matplotlib
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from matplotlib import font_manager, rc
+#한글표기
+font_path = 'C:/Windows/Fonts/malgun.ttf'
+font_name = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font_name)
+# 음수표기
+plt.rcParams['axes.unicode_minus'] = False
+
+#터미널 너비
+pd.set_option('display.width', 500)
+
+
+#=====================================================
+
+
+df = pd.read_excel('data/시도별_전출입_인구수.xlsx')
+
+print(df.head())
+
+df = df.ffill()
+print(df.head())
+
+
+# 전출지 = 서울, 전입지 = 서울빼고(전국) ==> 서울을 나간 사람들
+
+mask = (df['전출지별'] == '서울특별시') & (df['전입지별'] != '서울특별시')
+df_seoul = df[mask]
+df_seoul = df_seoul.drop(['전출지별'], axis=1)
+df_seoul = df_seoul.rename({'전입지별' : '전입지'}, axis=1)
+df_seoul = df_seoul.set_index('전입지')
+print(df_seoul)
+print()
+
+sr_one = df_seoul.loc['경기도']
+print(sr_one)
+print()
+
+#===================================
+
+plt.plot(sr_one)
+plt.show()
+
