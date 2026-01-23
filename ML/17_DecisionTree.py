9f61f5e3fea8b4504d8259768d1c1ad6d12004cb
cnsqo <cnsqo0815@gmail.com>
2026-01-23 16:51:49 +0900

    add
    add

---
+++ ML/17_DecisionTree.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+# wine = pd.read_csv('https://bit.ly/wine_csv_data')
+
+# wine.to_csv('./data/wine_data.csv', index=False)
+
+wine = pd.read_csv('./data/wine_data.csv')
+
+print(wine.head())
+print()
+wine.info()
+print()
+print(wine['class'].unique())  # 0 화이트와인 / 1 레드와인
+print()
+print(wine.describe())
+
+# 특성과 타깃 분리
+data = wine[['alcohol', 'sugar', 'pH']]
+target = wine['class']
+
+# 데이터 분할
+from sklearn.model_selection import train_test_split
+
+
+train_input, test_input, train_target, test_target = train_test_split(
+    data, target, test_size=0.2, random_state=42, stratify=target
+)
+
+# 스케일링
+from sklearn.preprocessing import StandardScaler
+
+ss = StandardScaler()
+train_scaled = ss.fit_transform(train_input)
+test_scaled = ss.transform(test_input)
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression()
+lr.fit(train_scaled, train_target)
+
+
+# 훈련 / 테스트 스코어
+
+print('\n========= 로지스틱 리그레션 훈련/ 테스트스코어 =========\n')
+print('훈련 스코어:', lr.score(train_scaled, train_target))
+print('테스트 스코어:', lr.score(test_scaled, test_target))
+
+# 파라미터 값 확인
+print('회귀 계수, 절편:', lr.coef_, lr.intercept_)
+print()
+
+# ======================================
+
+from sklearn.tree import DecisionTreeClassifier
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(train_scaled, train_target)
+
+# 훈련 / 테스트 스코어
+print('\n========= 결정나무 훈련/ 테스트스코어 =========\n')
+print('훈련 스코어:', dt.score(train_scaled, train_target))
+print('테스트 스코어:', dt.score(test_scaled, test_target))
+
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+
+plt.figure(figsize=(10,7))
+plot_tree(dt)
+plt.show()
+
